7718471f6aa66d8c128eb5a326f40306377bceb3
sam20gh <sam20gh@gmail.com>
2019-02-25 11:33:15 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(total = 0)
+    @total = total
+    @discount = discount = 20
+  end
+
+  def add_item(title, price)
+    self.price += total
+
+  end
+
+
+end
